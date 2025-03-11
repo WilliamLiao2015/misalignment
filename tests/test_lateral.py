1b5ce08543c07a7e06b5bda46eff9574b976b678
William Liao <williamliao2015@gmail.com>
2025-03-11 14:37:30 +0800

    refactor: Simplify test structure and enhance trajectory evaluation in benchmark tests

--- tests/test_lateral.py
+++ tests/test_lateral.py
@@ -1,36 +1,19 @@
-from shapely.geometry import LineString, Point, Polygon
+import numpy as np
 
 from .test_base import TestBase
 
 class TestLateral(TestBase):
-    def set_scenario(self, scenario):
-        super().set_scenario(scenario)
+    def test_going_straight(self):
+        start = self.trajectory[0]
+        end = self.trajectory[-1]
+        self.assertTrue(np.allclose(np.arctan2(end[:, 1]-start[:, 1], end[:, 0]-start[:, 0]), 0))
 
-        self.line_string = None
-        previous_id = None
+    def test_turning_right(self):
+        start = self.trajectory[0]
+        end = self.trajectory[-1]
+        self.assertTrue(np.all(np.arctan2(end[:, 1]-start[:, 1], end[:, 0]-start[:, 0]) > 0))
 
-        for t in range(1, len(self.trajectory)):
-            state = [*self.trajectory[t], 0]
-            lane = self.map.get_closest_lane(state)
-
-            if lane.id != previous_id:
-                line_string = LineString(lane.center.xy)
-                if self.line_string is None: self.line_string = line_string
-                else: self.line_string = self.line_string.union(line_string)
-                previous_id = lane.id
-
-    def get_x(self, t: int) -> float:
-        return self.line_string.project(Point(self.trajectory[t]), normalized=True)
-
-    def get_delta_x(self, t: int) -> float:
-        t = max(1, t)
-        return self.get_x(t) - self.get_x(t - 1)
-
-    def get_delta_v(self, t: int) -> float:
-        t = max(1, t)
-        print(self.get_delta_x(t), self.get_delta_x(t - 1))
-        return self.get_delta_x(t) - self.get_delta_x(t - 1)
-
-    def test_left_turn(self):
-        lane = self.map.get_closest_lane(self.trajectory[0])
-        polygon = Polygon(lane.center)
+    def test_turning_left(self):
+        start = self.trajectory[0]
+        end = self.trajectory[-1]
+        self.assertTrue(np.all(np.arctan2(end[:, 1]-start[:, 1], end[:, 0]-start[:, 0]) < 0))
--- tests/test_longitudinal.py
+++ tests/test_longitudinal.py
@@ -4,53 +4,17 @@ from .constants import epsilon_speed, epsilon_acceleration
 from .test_base import TestBase
 
 class TestLongitudinal(TestBase):
-    def get_speed(self, t: int) -> float:
-        t = max(1, t)
-        return np.linalg.norm(self.trajectory[t] - self.trajectory[t - 1])
-
-    def get_acceleration(self, t: int) -> float:
-        t = max(1, t)
-        return self.get_speed(t) - self.get_speed(t - 1)
-
-    def test_driving_forward(self):
-        # if self.get_x(len(self.trajectory) - 1) <= self.get_x(1):
-        #     self.fail(f"Trajectory does not move forward.")
-
-        for t in range(1, len(self.trajectory)):
-            delta_x = self.get_speed(t)
-            if delta_x <= -epsilon_speed:
-                self.fail(f"Trajectory moves backward of {delta_x} at {t}.")
-
-    def test_decelerating(self):
-        self.test_driving_forward()
-
-        if self.get_speed(len(self.trajectory) - 1) >= self.get_speed(1):
-            self.fail(f"Trajectory does not decelerate.")
-
-        for t in range(1, len(self.trajectory)):
-            delta_v = self.get_acceleration(t)
-            if delta_v > epsilon_acceleration:
-                self.fail(f"Speed increases of {delta_v} at {t}.")
-
     def test_cruising(self):
-        self.test_driving_forward()
-
-        # print(self.get_speed(len(self.trajectory) - 1), self.get_speed(1))
-        if abs(self.get_speed(len(self.trajectory) - 1) - self.get_speed(1)) > epsilon_speed:
-            self.fail(f"Trajectory does not cruise.")
-
-        for t in range(1, len(self.trajectory)):
-            delta_v = self.get_acceleration(t)
-            if abs(delta_v) > epsilon_acceleration:
-                self.fail(f"Speed changes of {delta_v} at {t}.")
+        self.assertTrue(np.all(np.abs(np.diff(self.trajectory[:, 1])) < epsilon_speed))
 
     def test_accelerating(self):
-        self.test_driving_forward()
+        self.assertTrue(np.all(np.diff(self.trajectory[:, 1]) > epsilon_acceleration))
 
-        if self.get_speed(len(self.trajectory) - 1) <= self.get_speed(1):
-            self.fail(f"Trajectory does not accelerate.")
+    def test_decelerating(self):
+        self.assertTrue(np.all(np.diff(self.trajectory[:, 1]) < -epsilon_acceleration))
 
-        for t in range(1, len(self.trajectory)):
-            delta_v = self.get_acceleration(t)
-            if delta_v < -epsilon_acceleration:
-                self.fail(f"Speed decreases of {delta_v} at {t}.")
+    def test_standing_still(self):
+        self.assertTrue(np.all(np.abs(self.trajectory[:, 1]) < epsilon_speed))
+
+    def test_reversing(self):
+        self.assertTrue(np.all(self.trajectory[:, 1] < 0))
