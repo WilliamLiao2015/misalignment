c91c2a7e536f7db265de519a2da2323f3848e675
William Liao <williamliao2015@gmail.com>
2025-03-12 15:09:16 +0800

    refactor: Enhance trajectory tests with epsilon thresholds for angles and speeds

--- tests/test_lateral.py
+++ tests/test_lateral.py
@@ -2,18 +2,18 @@ import numpy as np
 
 from .test_base import TestBase
 
+epsilon_straight = np.pi / 8
+epsilon_turning = np.pi / 4
+
 class TestLateral(TestBase):
     def test_going_straight(self):
-        start = self.trajectory[0]
-        end = self.trajectory[-1]
-        self.assertTrue(np.allclose(np.arctan2(end[:, 1]-start[:, 1], end[:, 0]-start[:, 0]), 0))
+        angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
+        self.assertTrue(np.all(np.abs(angles[1:] - angles[0]) < epsilon_straight))
 
     def test_turning_right(self):
-        start = self.trajectory[0]
-        end = self.trajectory[-1]
-        self.assertTrue(np.all(np.arctan2(end[:, 1]-start[:, 1], end[:, 0]-start[:, 0]) > 0))
+        angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
+        self.assertTrue(np.all(angles[1:] - angles[0] < epsilon_straight) and angles[-1] - angles[0] < -epsilon_turning)
 
     def test_turning_left(self):
-        start = self.trajectory[0]
-        end = self.trajectory[-1]
-        self.assertTrue(np.all(np.arctan2(end[:, 1]-start[:, 1], end[:, 0]-start[:, 0]) < 0))
+        angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
+        self.assertTrue(np.all(angles[1:] - angles[0] > -epsilon_straight) and angles[-1] - angles[0] > epsilon_turning)
--- tests/test_longitudinal.py
+++ tests/test_longitudinal.py
@@ -1,20 +1,23 @@
 import numpy as np
 
-from .constants import epsilon_speed, epsilon_acceleration
 from .test_base import TestBase
 
+epsilon_speed = 0.1
+epsilon_acceleration = 0.05
+
 class TestLongitudinal(TestBase):
+    def test_decelerating(self):
+        deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
+        self.assertTrue(np.all(deltas[1:] - deltas[0] < epsilon_acceleration))
+
     def test_cruising(self):
-        self.assertTrue(np.all(np.abs(np.diff(self.trajectory[:, 1])) < epsilon_speed))
+        deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
+        self.assertTrue(np.all(np.abs(deltas[1:] - deltas[0]) < epsilon_acceleration))
 
     def test_accelerating(self):
-        self.assertTrue(np.all(np.diff(self.trajectory[:, 1]) > epsilon_acceleration))
-
-    def test_decelerating(self):
-        self.assertTrue(np.all(np.diff(self.trajectory[:, 1]) < -epsilon_acceleration))
+        deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
+        self.assertTrue(np.all(deltas[1:] - deltas[0] > -epsilon_acceleration))
 
     def test_standing_still(self):
-        self.assertTrue(np.all(np.abs(self.trajectory[:, 1]) < epsilon_speed))
-
-    def test_reversing(self):
-        self.assertTrue(np.all(self.trajectory[:, 1] < 0))
+        deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
+        self.assertTrue(np.all(deltas < epsilon_speed))
