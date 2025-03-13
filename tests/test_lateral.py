e51166bc9862abab6c98fa7f1fff7b7d849cf45c
William Liao <williamliao2015@gmail.com>
2025-03-13 18:08:41 +0800

    fix: Correct angle difference calculations in lateral tests to handle wrap-around

--- tests/test_lateral.py
+++ tests/test_lateral.py
@@ -8,15 +8,15 @@ epsilon_turning = np.pi / 6
 class TestLateral(TestBase):
     def test_going_straight(self):
         angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        diffs = angles[1:] - angles[0]
+        diffs = (angles[1:] - angles[0]) % np.pi
         self.assertTrue(np.all(np.abs(diffs) < epsilon_straight), f"Invalid differences: {diffs[np.abs(diffs) >= epsilon_straight]}")
 
     def test_turning_right(self):
         angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        diffs = angles[1:] - angles[0]
+        diffs = (angles[1:] - angles[0]) % np.pi
         self.assertTrue(np.all(diffs < epsilon_straight) and diffs[-1] < -epsilon_turning, f"Invalid differences: {diffs[(diffs >= epsilon_straight)]}, {diffs[-1]}")
 
     def test_turning_left(self):
         angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        diffs = angles[1:] - angles[0]
+        diffs = (angles[1:] - angles[0]) % np.pi
         self.assertTrue(np.all(diffs > -epsilon_straight) and diffs[-1] > epsilon_turning, f"Invalid differences: {diffs[(diffs <= -epsilon_straight)]}, {diffs[-1]}")
