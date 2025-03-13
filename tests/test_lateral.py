0053d6fc145ab5b8b53ec9df7cded0da7177c9b4
William Liao <williamliao2015@gmail.com>
2025-03-13 13:11:47 +0800

    refactor: Update epsilon thresholds and enhance error messaging in lateral tests

--- tests/test_lateral.py
+++ tests/test_lateral.py
@@ -2,18 +2,21 @@ import numpy as np
 
 from .test_base import TestBase
 
-epsilon_straight = np.pi / 8
-epsilon_turning = np.pi / 4
+epsilon_straight = np.pi / 12
+epsilon_turning = np.pi / 6
 
 class TestLateral(TestBase):
     def test_going_straight(self):
         angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        self.assertTrue(np.all(np.abs(angles[1:] - angles[0]) < epsilon_straight))
+        diffs = angles[1:] - angles[0]
+        self.assertTrue(np.all(np.abs(diffs) < epsilon_straight), f"Invalid differences: {diffs[np.abs(diffs) >= epsilon_straight]}")
 
     def test_turning_right(self):
         angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        self.assertTrue(np.all(angles[1:] - angles[0] < epsilon_straight) and angles[-1] - angles[0] < -epsilon_turning)
+        diffs = angles[1:] - angles[0]
+        self.assertTrue(np.all(diffs < epsilon_straight) and diffs[-1] < -epsilon_turning, f"Invalid differences: {diffs[(diffs >= epsilon_straight)]}, {diffs[-1]}")
 
     def test_turning_left(self):
         angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        self.assertTrue(np.all(angles[1:] - angles[0] > -epsilon_straight) and angles[-1] - angles[0] > epsilon_turning)
+        diffs = angles[1:] - angles[0]
+        self.assertTrue(np.all(diffs > -epsilon_straight) and diffs[-1] > epsilon_turning, f"Invalid differences: {diffs[(diffs <= -epsilon_straight)]}, {diffs[-1]}")
