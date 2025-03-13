a22788632167143877501f85b0d10320df65043f
William Liao <williamliao2015@gmail.com>
2025-03-13 13:12:19 +0800

    refactor: Update `set_trajectory` method to use `numpy.ndarray` type hint

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,7 @@
 import unittest
 
+import numpy as np
+
 class TestBase(unittest.TestCase):
-    def set_trajectory(self, trajectory):
+    def set_trajectory(self, trajectory: np.ndarray):
         self.trajectory = trajectory
