558814d02416b2691eae46da0ddcf5b9513c9dec
William Liao <williamliao2015@gmail.com>
2025-03-14 12:10:56 +0800

    refactor: Adjust angle calculations and differences in lateral and longitudinal tests

--- tests/test_lateral.py
+++ tests/test_lateral.py
@@ -7,16 +7,16 @@ epsilon_turning = np.pi / 6
 
 class TestLateral(TestBase):
     def test_going_straight(self):
-        angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        diffs = (angles[1:] - angles[0]) % np.pi
+        angles = np.asarray([np.arctan2(y2 - y1, x2 - x1) for (x1, y1), (x2, y2) in zip(self.trajectory[0, :-1], self.trajectory[0, 1:])])
+        diffs = (angles[1:] - angles[0] + np.pi) % (2 * np.pi) - np.pi
         self.assertTrue(np.all(np.abs(diffs) < epsilon_straight), f"Invalid differences: {diffs[np.abs(diffs) >= epsilon_straight]}")
 
     def test_turning_right(self):
-        angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        diffs = (angles[1:] - angles[0]) % np.pi
+        angles = np.asarray([np.arctan2(y2 - y1, x2 - x1) for (x1, y1), (x2, y2) in zip(self.trajectory[0, :-1], self.trajectory[0, 1:])])
+        diffs = (angles[1:] - angles[0] + np.pi) % (2 * np.pi) - np.pi
         self.assertTrue(np.all(diffs < epsilon_straight) and diffs[-1] < -epsilon_turning, f"Invalid differences: {diffs[(diffs >= epsilon_straight)]}, {diffs[-1]}")
 
     def test_turning_left(self):
-        angles = np.asarray([np.arctan2(y - self.trajectory[0, 0, 1], x - self.trajectory[0, 0, 0]) for x, y in self.trajectory[0, 1:, :]])
-        diffs = (angles[1:] - angles[0]) % np.pi
+        angles = np.asarray([np.arctan2(y2 - y1, x2 - x1) for (x1, y1), (x2, y2) in zip(self.trajectory[0, :-1], self.trajectory[0, 1:])])
+        diffs = (angles[1:] - angles[0] + np.pi) % (2 * np.pi) - np.pi
         self.assertTrue(np.all(diffs > -epsilon_straight) and diffs[-1] > epsilon_turning, f"Invalid differences: {diffs[(diffs <= -epsilon_straight)]}, {diffs[-1]}")
--- tests/test_longitudinal.py
+++ tests/test_longitudinal.py
@@ -3,22 +3,22 @@ import numpy as np
 from .test_base import TestBase
 
 epsilon_speed = 0.1
-epsilon_acceleration = 0.05
+epsilon_acceleration = 0.1
 
 class TestLongitudinal(TestBase):
     def test_decelerating(self):
         deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
-        diffs = deltas[1:] - deltas[0]
+        diffs = deltas[1:] - deltas[:-1]
         self.assertTrue(np.all(diffs < epsilon_acceleration), f"Invalid differences: {diffs[diffs >= epsilon_acceleration]}")
 
     def test_cruising(self):
         deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
-        diffs = deltas[1:] - deltas[0]
+        diffs = deltas[1:] - deltas[:-1]
         self.assertTrue(np.all(np.abs(diffs) < epsilon_acceleration), f"Invalid differences: {diffs[np.abs(diffs) >= epsilon_acceleration]}")
 
     def test_accelerating(self):
         deltas = np.linalg.norm(np.diff(self.trajectory[0], axis=0), axis=1)
-        diffs = deltas[1:] - deltas[0]
+        diffs = deltas[1:] - deltas[:-1]
         self.assertTrue(np.all(diffs > -epsilon_acceleration), f"Invalid differences: {diffs[diffs <= -epsilon_acceleration]}")
 
     def test_standing_still(self):
